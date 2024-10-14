f22c9a03f0e162975510dcd0014239fce3b32a0e
Jafar Hasan <se.hasanjafar@gmail.com>
2024-10-14 08:45:04 +0530

    Push1

---
+++ src/main/java/com/ShowAPIs/ShowAPI/ShowApiApplication.java
@@ -0,0 +1,13 @@
+package com.ShowAPIs.ShowAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShowAPIs/ShowAPI/ShowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShowAPIs.ShowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
